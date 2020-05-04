117c6edf3d10f042904352e9bdc3be85aa419038
azdoolittle <drewdoolittle@gmail.com>
2020-05-04 11:39:50 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Product p = new Product();
+        Book b = new Book();
+        Software s = new Software();
+
+        System.out.println("product: \n" + p);
+        System.out.println("book: \n" + b);
+        System.out.println("software: \n" + s);
+    }
+}
---
+++ src/Software.java
@@ -0,0 +1,9 @@
+public class Software extends Product {
+
+    private String programmer;
+    private String platform; // linux, mac, or pc
+    private String os;
+
+
+
+}
